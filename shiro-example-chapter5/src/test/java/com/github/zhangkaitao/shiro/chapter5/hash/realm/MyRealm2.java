68aaf070ccec0eab993f7e1c9ad581b70b774e46
闪狙手 <s0181@163.com>
2021-05-06 07:58:56 +0800

    M nothing diff

--- shiro-example-chapter5/src/test/java/com/github/zhangkaitao/shiro/chapter5/hash/realm/MyRealm2.java
+++ shiro-example-chapter5/src/test/java/com/github/zhangkaitao/shiro/chapter5/hash/realm/MyRealm2.java
@@ -27,6 +27,7 @@ public class MyRealm2 extends AuthorizingRealm {
         String username = "liu"; //用户名及salt1
         String salt2 = "0072273a5d87322163795118fdd7c45e";
         String password = "be320beca57748ab9632c4121ccac0db"; //加密后的密码
+        System.out.println("token = " + token);
         SimpleAuthenticationInfo ai = new SimpleAuthenticationInfo(username, password, getName());
         ai.setCredentialsSalt(ByteSource.Util.bytes(username+salt2)); //盐是用户名+随机数
         return ai;
